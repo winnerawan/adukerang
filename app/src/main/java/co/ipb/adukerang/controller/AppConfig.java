a8553a88275e28162e81d4f5fb9cbc60d61fff7c
winnerawan <third1337@gmail.com>
2016-03-23 16:55:07 +0700

    Rebuild From Scratch

---
+++ app/src/main/java/co/ipb/adukerang/controller/AppConfig.java
@@ -0,0 +1,9 @@
+package co.ipb.adukerang.controller;
+
+/**
+ * Created by winnerawan on 3/23/16.
+ */
+public class AppConfig {
+
+    public static String URL_REGIST="http://api.winnerawan.net/";
+}
